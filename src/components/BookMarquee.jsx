9a59b5b61a2170e181450fc4d7f15f92d89265b1
Nayan Dey <ndeynayan123@gmail.com>
2026-04-30 23:04:48 +0530

    added and designed book marquee

--- src/components/BookMarquee.jsx
+++ src/components/BookMarquee.jsx
@@ -13,13 +13,34 @@ const BookMarquee = () => {
 
 
     return (
-        <div>
-            <Marquee>
-                {
-                    book.map(b => <span key={book.id}>New Arrivals:{b.title} || Special Discount on Memberships...</span>)
-                }
+        <div className="bg-linear-to-r from-blue-50 via-white to-purple-50 border-y border-gray-200 py-2 my-10">
+            <Marquee pauseOnHover={true} speed={60}>
+
+                {book.map((b) => (
+                    <span
+                        key={b.id}
+                        className="mx-10 text-gray-700 text-lg flex items-center gap-2"
+                    >
+                        <span className="text-blue-600 font-semibold">
+                             New Arrivals:
+                        </span>
+                        <span className="font-medium text-gray-800">
+                            {b.title}
+                        </span>
+                    </span>
+                ))}
+
+                <span>|</span>
+
+                <span className="mx-10 text-lg">
+                    <span className="text-purple-600 font-semibold">
+                         Special Discount on Memberships...
+                    </span>
+                </span>
+
             </Marquee>
         </div>
+
     );
 };
 
---
+++ src/components/FeaturedBooks.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const FeaturedBooks = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FeaturedBooks;
