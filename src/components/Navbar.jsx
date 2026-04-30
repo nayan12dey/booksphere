250bdfead75718a6e921d845b7196199c6fab835
Nayan Dey <ndeynayan123@gmail.com>
2026-04-30 15:13:53 +0530

    added navbar and designed with logo and login button

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+
+import { Button } from '@heroui/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="border-b px-2">
+            <nav className=" flex justify-between items-center py-3 max-w-7xl mx-auto w-full">
+                <div>
+                    <Link href={"/"} className='flex gap-2 items-center'>
+                        <Image
+                            src={"/logo.avif"}
+                            alt="logo"
+                            loading="eager"
+                            width={30}
+                            height={30}
+                            className="object-cover h-auto w-auto"
+                        />
+                        <h3 className="font-black text-lg">BookSphere</h3>
+                    </Link>
+                </div>
+
+                <ul className="flex items-center gap-5 text-sm">
+                    <li>
+                        <Link href={"/"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link href={"/all-books"}>All Books</Link>
+                    </li>
+                </ul>
+
+                <div className="flex gap-4">
+                    <Button variant='danger'>Login</Button>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
